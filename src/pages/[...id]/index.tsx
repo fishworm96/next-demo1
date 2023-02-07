e73f55223bbe325fe5bf1a27e537bcd9ccf41d73
fishworm96 <245525860@qq.com>
2023-02-07 20:34:42 +0800

    feat: 单Layout

--- src/pages/[...id]/index.tsx
+++ src/pages/[...id]/index.tsx
@@ -1,11 +1,12 @@
 import { useRouter } from "next/router"
+import classes from "./slug.module.css"
 
 const Slug = () => {
   const router = useRouter()
   const { id } = router.query
   console.log(id, router.pathname)
 
-  return <div>{id}</div>
+  return <div className={classes.slug}>{id}</div>
 }
 
 export default Slug
