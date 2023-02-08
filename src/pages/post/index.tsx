b6bb2577582a34109e3af30695cc476fda8e079e
fishworm96 <245525860@qq.com>
2023-02-08 22:01:53 +0800

    feat: 使用api获取数据

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,5 +1,17 @@
-const Post = () => {
-  return <h1>Post</h1>
+import { InferGetStaticPropsType } from "next"
+
+const Post = ({ name }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  console.log(name)
+  return <h1>{name}</h1>
+}
+
+export const getStaticProps = async () => {
+  const result = await fetch("http://localhost:3000/api/hello")
+  const data = await result.json()
+
+  return {
+    props: data
+  }
 }
 
 export default Post
