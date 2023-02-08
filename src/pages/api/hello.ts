37a21570f4756e7bff5c89f1ef7568bdf5524e00
fishworm96 <245525860@qq.com>
2023-02-08 21:44:43 +0800

    使用api

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -5,9 +5,10 @@ type Data = {
   name: string
 }
 
+// 返回一个http请求，http://localhost:3000/api/hello
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe1' })
 }
